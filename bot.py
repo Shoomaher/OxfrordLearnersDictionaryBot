5aa200953fb4caed8ee3e80dccbca43bde4bc5b3
Mikhail <mihail.shoomaher@gmail.com>
2017-06-09 01:50:41 +0300

    Create bot.py

--- bot.py
+++ bot.py
@@ -6,14 +6,10 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
-	bot.send_message(message.chat.id, 'Привет! Пришли мне слово и я найду его тебе')
+bot.send_message(message.chat.id, 'Hi! Tell me the word and I\'ll send you the definition')
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-	'''
-	Тут будем слать http запрос, парсить его и присылать ответ
-	'''
-	
 	bot.send_message(message.chat.id, parser.search(message.text))
 
 if __name__ == '__main__':
