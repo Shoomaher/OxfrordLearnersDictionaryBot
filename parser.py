f06c4588eeaff39e02124ceebedda9787dac53c0
Mikhail <mihail.shoomaher@gmail.com>
2017-06-09 22:54:10 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,31 +1,50 @@
-import urllib.request
+import urllib
 import config
 from bs4 import BeautifulSoup
+code=0
 
 def get_url(word):
 	word = word.lower()
-	URL = config.url + word + '_1?q=' + word
+	if word.find('-') != -1:
+		word = word.replace(' ', '-')
+		URL = config.url + word + '_1?q=' + word
+	else:
+		word = word.replace(' ', '-')
+		URL = config.url + word + '_2?q=' + word
 	return URL
 
 def get_html(url):
-	response = urllib.request.urlopen(url)
-	return response.read()
+	global code
+	try:
+		response = urllib.request.urlopen(url)
+		respRead = response.read()
+	except urllib.error.HTTPError as err:
+		print(err.code)
+		code = err.code
+		return 0
+	return respRead
 
 def parse(html):
-	soup = BeautifulSoup(html, "html.parser")
-	list = soup.find('div', class_='entry')
-	for span in soup.findAll('span', 'idm-g'):
-		span.extract()
-	definitions = list.find_all('span', class_= 'def')
-	defs = []
-	for defin in definitions:
-		defs.append(defin.text)
-	definition = ' '
-	i=1
-	for d in defs:
-		d = d[0].upper() + d[1:]
-		definition+= str(i) + '. ' + d +'\n'
-		i+=1 
-	return definition
+	global code
+	if code == 0:
+		soup = BeautifulSoup(html, "html.parser")
+		list = soup.find('div', class_='entry')
+		for span in soup.findAll('span', 'idm-g'):
+			span.extract()
+		definitions = list.find_all('span', class_= 'def')
+		defs = []
+		for defin in definitions:
+			defs.append(defin.text)
+		definition = ' '
+		i=1
+		for d in defs:
+			d = d.strip()
+			d = d[0].upper() + d[1:]
+			definition+= str(i) + '. ' + d +'\n'
+			i+=1 
+		return definition
+	else:
+		code=0
+		return 'Что-то пошло не так!'
 def search(WORD):
 	return parse(get_html(get_url(WORD)))
