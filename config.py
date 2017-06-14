450e7f2aa04991db119605f4d8814a9478881b37
Mikhail <mihail.shoomaher@gmail.com>
2017-06-14 20:44:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 token = 'Insert your token here'
 url = 'https://www.oxfordlearnersdictionaries.com/definition/english/'
+parts_of_speech = ['auxiliary verb', 'phrasal verb', 'adverb', 'verb', 'adjective', 'stative', 'numeral', 'conjunction', 'preposition', 'particle', 'interjection', 'exclamation', 'modal', 'noun', 'pronoun']
